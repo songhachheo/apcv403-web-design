cf6f65eff36f67ff33fc4d0deeb4b3aac8458ea5
cloudySky <cloudyskyyy@protonmail.com>
2022-02-27 16:15:53 -0700

    Added completed Assignment 6 content

---
+++ assignment6_javascript/assignment6_javascript.js
@@ -0,0 +1,48 @@
+/*
+Modify this JavaScript program to display time in the following format:
+Today is: 3 rd Day of the Week
+Current Time: 10: 30: 38 PM
+*/
+
+var today = new Date();
+var day = today.getDay();
+var nDay = day + 1;
+var daylist = ["st", "nd", "rd", "th"];
+
+if (nDay == 1) {
+    console.log("Today is: " + nDay + " " + daylist[day] + " Day of the Week");
+} else if (nDay == 2) {
+    console.log("Today is: " + nDay + " " + daylist[day] + " Day of the Week");
+} else if (nDay == 3) {
+    console.log("Today is: " + nDay + " " + daylist[day] + " Day of the Week");
+} else if (nDay > 3 && nDay < 8) {
+    console.log("Today is: " + nDay + " " + daylist[3] + " Day of the Week");
+}
+
+var hour = today.getHours();
+var minute = today.getMinutes();
+var second = today.getSeconds();
+var prepand = hour >= 12 ? " PM " : " AM ";
+hour = hour >= 12 ? hour - 12 : hour;
+if (hour === 0 && prepand === " PM ") {
+    if (minute === 0 && second === 0) {
+        hour = 12;
+        prepand = " Noon";
+    } else {
+        hour = 12;
+        prepand = " PM";
+    }
+}
+if (hour === 0 && prepand === " AM ") {
+    if (minute === 0 && second === 0) {
+        hour = 12;
+        prepand = " Midnight";
+    } else {
+        hour = 12;
+        prepand = " AM";
+    }
+}
+
+console.log(
+    "Current Time: " + hour + ": " + minute + ": " + second + prepand
+);
